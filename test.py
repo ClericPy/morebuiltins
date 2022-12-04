98f8b9fbbd23d2f3f7fcc2917c940f677da09616
Clericpy <clericpy@gmail.com>
2022-12-04 17:58:25 +0800

    DomainParser

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
+import doctest
+
+import morebuiltins.urllibs
 import morebuiltins.utils
 
 if __name__ == "__main__":
-    import doctest
 
     doctest.testmod(morebuiltins.utils)
+    doctest.testmod(morebuiltins.urllibs)
