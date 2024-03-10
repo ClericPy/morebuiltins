060dcb51b11d8771525b80af5c36e01df066f80c
Clericpy <clericpy@gmail.com>
2024-03-10 21:44:21 +0800

    fix python3.7 partial callable staticmethod

--- morebuiltins/_urllib.py
+++ morebuiltins/_urllib.py
@@ -19,6 +19,109 @@ __all__ = [
 ]
 
 
+def _request(
+    url: str,
+    params: Optional[dict] = None,
+    headers: Optional[dict] = None,
+    data: Optional[bytes] = None,
+    json=None,
+    form=None,
+    timeout=None,
+    method: str = "GET",
+    verify=True,
+    encoding=None,
+    urlopen_kwargs: Optional[dict] = None,
+    **kwargs,
+):
+    if params:
+        if "?" in url:
+            sep = "&"
+        else:
+            sep = "?"
+        url += f"{sep}{urlencode(params)}"
+    if headers is None:
+        headers = {}
+    if data:
+        if isinstance(data, bytes):
+            headers.setdefault("Content-Type", "")
+        elif isinstance(data, dict):
+            # as form
+            data = urlencode(data, doseq=True).encode("utf-8")
+            headers.setdefault("Content-Type", "application/x-www-form-urlencoded")
+    else:
+        if json:
+            data = _json.dumps(json, allow_nan=False).encode("utf-8")
+            headers.setdefault("Content-Type", "application/json")
+        elif form:
+            data = urlencode(form, doseq=True).encode("utf-8")
+            headers.setdefault("Content-Type", "application/x-www-form-urlencoded")
+    req = Request(method=method, url=url, headers=headers, data=data, **kwargs)
+    if urlopen_kwargs is None:
+        urlopen_kwargs = {}
+    urlopen_kwargs["url"] = req
+    if timeout is not None:
+        urlopen_kwargs.setdefault("timeout", timeout)
+    if not verify:
+        ctx = urlopen_kwargs.setdefault("context", ssl.create_default_context())
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
+
+    def ensure_attrs():
+        if hasattr(HTTPResponse, "get_encoding"):
+            return True
+
+        def get_body(self):
+            return getattr(self, "_response_body", b"")
+
+        def get_encoding(self):
+            charset = re.search(rb"charset[\s\"'=]*([a-zA-Z0-9._-]+)", self.content)
+            if charset:
+                encoding = charset.group(1).decode("utf-8", "ignore")
+            else:
+                encoding = "utf-8"
+            return encoding
+
+        def get_text(self):
+            if self.encoding is None:
+                encoding = self.get_encoding()
+            else:
+                encoding = self.encoding
+            return self.content.decode(encoding, "replace")
+
+        def get_json(self, **kwargs):
+            return _json.loads(self.content, **kwargs)
+
+        def get_code(self):
+            return self.code
+
+        def get_ok(self):
+            return self.code < 400
+
+        for cls in [HTTPResponse, HTTPError]:
+            # monkey patch
+            setattr(cls, "encoding", None)
+            setattr(cls, "status_code", property(get_code))
+            setattr(cls, "content", property(get_body))
+            setattr(cls, "text", property(get_text))
+            setattr(cls, "ok", property(get_ok))
+            setattr(cls, "json", get_json)
+            setattr(cls, "get_encoding", get_encoding)
+
+    ensure_attrs()
+    response = None
+    try:
+        with urlopen(**urlopen_kwargs) as resp:
+            body = resp.read()
+            response = resp
+    except HTTPError as error:
+        body = error.read()
+        response = error
+    setattr(response, "_response_body", body)
+    setattr(response, "encoding", encoding)
+    setattr(response, "headers", dict(response.headers))
+    return response
+
+
 class req:
     """A simple requests mock, slow but useful.
 
@@ -42,116 +145,14 @@ class req:
 
     RequestErrors = (URLError,)
 
-    @staticmethod
-    def request(
-        url: str,
-        params: Optional[dict] = None,
-        headers: Optional[dict] = None,
-        data: Optional[bytes] = None,
-        json=None,
-        form=None,
-        timeout=None,
-        method: str = "GET",
-        verify=True,
-        encoding=None,
-        urlopen_kwargs: Optional[dict] = None,
-        **kwargs,
-    ):
-        if params:
-            if "?" in url:
-                sep = "&"
-            else:
-                sep = "?"
-            url += f"{sep}{urlencode(params)}"
-        if headers is None:
-            headers = {}
-        if data:
-            if isinstance(data, bytes):
-                headers.setdefault("Content-Type", "")
-            elif isinstance(data, dict):
-                # as form
-                data = urlencode(data, doseq=True).encode("utf-8")
-                headers.setdefault("Content-Type", "application/x-www-form-urlencoded")
-        else:
-            if json:
-                data = _json.dumps(json, allow_nan=False).encode("utf-8")
-                headers.setdefault("Content-Type", "application/json")
-            elif form:
-                data = urlencode(form, doseq=True).encode("utf-8")
-                headers.setdefault("Content-Type", "application/x-www-form-urlencoded")
-        req = Request(method=method, url=url, headers=headers, data=data, **kwargs)
-        if urlopen_kwargs is None:
-            urlopen_kwargs = {}
-        urlopen_kwargs["url"] = req
-        if timeout is not None:
-            urlopen_kwargs.setdefault("timeout", timeout)
-        if not verify:
-            ctx = urlopen_kwargs.setdefault("context", ssl.create_default_context())
-            ctx.check_hostname = False
-            ctx.verify_mode = ssl.CERT_NONE
-
-        def ensure_attrs():
-            if hasattr(HTTPResponse, "get_encoding"):
-                return True
-
-            def get_body(self):
-                return getattr(self, "_response_body", b"")
-
-            def get_encoding(self):
-                charset = re.search(rb"charset[\s\"'=]*([a-zA-Z0-9._-]+)", self.content)
-                if charset:
-                    encoding = charset.group(1).decode("utf-8", "ignore")
-                else:
-                    encoding = "utf-8"
-                return encoding
-
-            def get_text(self):
-                if self.encoding is None:
-                    encoding = self.get_encoding()
-                else:
-                    encoding = self.encoding
-                return self.content.decode(encoding, "replace")
-
-            def get_json(self, **kwargs):
-                return _json.loads(self.content, **kwargs)
-
-            def get_code(self):
-                return self.code
-
-            def get_ok(self):
-                return self.code < 400
-
-            for cls in [HTTPResponse, HTTPError]:
-                # monkey patch
-                setattr(cls, "encoding", None)
-                setattr(cls, "status_code", property(get_code))
-                setattr(cls, "content", property(get_body))
-                setattr(cls, "text", property(get_text))
-                setattr(cls, "ok", property(get_ok))
-                setattr(cls, "json", get_json)
-                setattr(cls, "get_encoding", get_encoding)
-
-        ensure_attrs()
-        response = None
-        try:
-            with urlopen(**urlopen_kwargs) as resp:
-                body = resp.read()
-                response = resp
-        except HTTPError as error:
-            body = error.read()
-            response = error
-        setattr(response, "_response_body", body)
-        setattr(response, "encoding", encoding)
-        setattr(response, "headers", dict(response.headers))
-        return response
-
-    get = partial(request, method="GET")
-    head = partial(request, method="HEAD")
-    post = partial(request, method="POST")
-    put = partial(request, method="PUT")
-    delete = partial(request, method="DELETE")
-    patch = partial(request, method="PATCH")
-    options = partial(request, method="OPTIONS")
+    request = _request
+    get = partial(_request, method="GET")
+    head = partial(_request, method="HEAD")
+    post = partial(_request, method="POST")
+    put = partial(_request, method="PUT")
+    delete = partial(_request, method="DELETE")
+    patch = partial(_request, method="PATCH")
+    options = partial(_request, method="OPTIONS")
 
 
 class DomainParser(object):
